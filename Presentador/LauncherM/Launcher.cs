3c126dea7aa170581f812c6cb355d4d7d5fc14c4
juan carlos chaparro vera 10092020_1447 <juanchvere@hotmail.com>
2020-09-16 16:15:55 -0500

    Crea Base proyecto

---
+++ Presentador/LauncherM/Launcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LauncherM
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Presentador/Presentador/Vista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentador
+{
+    public partial class Vista : Form
+    {
+        public Vista()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
